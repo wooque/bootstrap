110cce3af0567776be6505873038dda24b56835f
Vuk Mirovic <wooque@gmail.com>
2016-02-23 10:14:43 +0100

    Added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo yum -y install zsh git vim
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git init
+git remote add origin https://github.com/wooque/configs
+git fetch --all
+git reset --hard origin/master
+git branch --set-upstream-to=origin/master master
