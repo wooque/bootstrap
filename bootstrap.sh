b9454a8dd6b26a2265b73efe0905798fa7801504
Vuk Mirovic <wooque@gmail.com>
2016-09-20 22:46:54 +0200

    No need for lsb_release more

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo yum -y install zsh git vim redhat-lsb-core
+sudo yum -y install zsh git vim
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git init
 git remote add origin https://github.com/wooque/configs
