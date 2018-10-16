66fdbf9dded8fd8dc4302b9b9ad45be2266e0d90
Vuk Mirovic <wooque@gmail.com>
2018-10-16 18:46:05 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,15 @@ elif type yum &> /dev/null ; then
     PKGR=yum
 else
     echo "No package manager"
+    exit
 fi
 
 sudo $PKGR -y install zsh git vim htop ranger highlight
+
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 git init
 git remote add origin https://github.com/wooque/configs
 git fetch --all
 git reset --hard origin/minimal
-git branch --set-upstream-to=origin/minimal minimal
+git checkout minimal
