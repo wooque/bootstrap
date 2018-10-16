61f47367c88bd2e0597621fbb61336f168e53c13
Vuk Mirovic <wooque@gmail.com>
2018-10-16 18:00:00 +0200

    Fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ else
     echo "No package manager"
 fi
 
-sudo $PKGR -y install zsh git vim htop ranger hihglight
+sudo $PKGR -y install zsh git vim htop ranger highlight
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git init
 git remote add origin https://github.com/wooque/configs
